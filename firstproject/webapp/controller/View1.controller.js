aa0b92a8c3630989c823ef23bad89118f17b39af
heyapurv <deskofapurv@gmail.com>
2026-06-10 10:23:05 +0000

    Simple form project

--- firstproject/webapp/controller/View1.controller.js
+++ firstproject/webapp/controller/View1.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("com.demo.firstproject.controller.View1", {
@@ -12,11 +13,23 @@ sap.ui.define([
         },
         onSubmit: function () {
             var name = this.getView().byId("nameInput").getValue();
+            var mobile = this.getView().byId("mobileInput").getValue();
+            var email = this.getView().byId("emailInput").getValue();
             this.getView().byId("heading").setText(name)
             this.getView().byId("submitbtn").setType("Accept")
             this.getView().byId("heading").setTextAlign("Left")
             this.getView().byId("nameInput").setEnabled(false)
+            this.getView().byId("nameData").setText("Name : "+name)
+            this.getView().byId("mobileData").setText("Mobile : "+mobile)
+            this.getView().byId("emailData").setText("Email : "+email)
+            
+            // submit message 
+            sap.m.MessageToast.show("Thank You " + name);
 
+            // clear input fields 
+            this.getView().byId("nameInput").setValue("")
+            this.getView().byId("mobileInput").setValue("")
+            this.getView().byId("emailInput").setValue("")
         }
     });
 });
